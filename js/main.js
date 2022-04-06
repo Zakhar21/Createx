b19e23149f59aa5d3a983fcf8068ee558f289c21
Zakhar21 <ip609108@gmail.com>
2022-04-06 22:18:52 +0700

    Доработал раздел с тегами

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 
 AOS.init();
-// var mixer = mixitup('.directions__list');
+var mixer = mixitup('.directions__list',{
+    controls: {
+        toggleLogic: 'and',
+        toggleDefault: 'none'
+      },
+      animation: {
+        duration: 300
+      }
+});
+
+
+// Удаляем класс у прежнего элемента
+let btns = document.querySelectorAll('.directions__filter-btn');
+btns.forEach(item => {
+    item.addEventListener('click', (e) => {
+        btns.forEach(el=>{
+            el.classList.remove('directions__filter-btn--active')
+        });
+        item.classList.add('directions__filter-btn--active')
+    });
+});
+
