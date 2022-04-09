48304a47687bb6219399cf8880807469fcd83769
Zakhar21 <ip609108@gmail.com>
2022-04-09 12:55:25 +0700

    Слайдеры

--- js/main.js
+++ js/main.js
@@ -21,4 +21,39 @@ btns.forEach(item => {
         item.classList.add('directions__filter-btn--active')
     });
 });
+// слайдер 
+$('.team__slider').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow:4,
+    draggable:false,
+    infinity:true,
+    appendArrows: $('.team__slider-arrows'),
+    waitForAnimate:false,
+});
 
+$('.team__slider-prev').on('click', function(e){
+    e.preventDefault();
+    $('.team__slider').slick('slickPrev');
+});
+$('.team__slider-next').on('click', function(e){
+    e.preventDefault();
+    $('.team__slider').slick('slickNext');
+});
+
+
+$('.testimonials__slider').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow:1,
+    infinity:true,
+    waitForAnimate:false,
+});
+$('.testimonials__prev').on('click', function(e){
+    e.preventDefault();
+    $('.testimonials__slider').slick('slickPrev');
+});
+$('.testimonials__next').on('click', function(e){
+    e.preventDefault();
+    $('.testimonials__slider').slick('slickNext');
+});
